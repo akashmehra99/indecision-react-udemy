1c5c249c0bf5f1441c54f5c98a6ba730012334b6
Akash MEHRA <akash.mehra99@gmail.com>
2020-04-07 23:30:32 +0530

    build changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'docs'),
         filename: "bundle.js"
     },
     module: {
@@ -25,7 +25,7 @@ module.exports = {
     },
     devtool: 'eval-cheap-module-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
+        contentBase: path.join(__dirname, 'docs'),
         compress: true,
         port: 9000
     }
